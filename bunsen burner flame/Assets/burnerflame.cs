f0a0c334f29a470bd8fe0f518d1855360ecf5f08
noraishahMusa <67356735+noraishahMusa@users.noreply.github.com>
2020-06-24 15:07:59 +0800

    add empty project

---
+++ bunsen burner flame/Assets/burnerflame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burnerflame : MonoBehaviour
+{
+    private ParticleSystem Bflame;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bflame.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) ;
+        {
+            Bflame.Play();
+        }
+        if (Input.GetMouseButtonUp(0)) ;
+        {
+            Bflame.Stop();
+        }
+    }
+}
